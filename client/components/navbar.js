95d74f389a72f9f54aff58a9204b23552ce8a944
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-14 13:54:00 -0500

    some debugging

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>Stackathon App</h1>
+    <h1>code-share App</h1>
     <nav>
       {isLoggedIn ? (
         <div>
