58ea31d028e6619893ae5ebf1125d7a4c41c11a9
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-15 11:09:00 -0500

    most basic text box in react

--- client/components/InputBox.js
+++ client/components/InputBox.js
@@ -1,11 +1,58 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+//import PropTypes from 'prop-types'
+//import {connect} from 'react-redux'
 
-export const InputBox = props => {
-  return (
-    <div>
-      <h3>Not yet an input box but I exist to try and test if sharedb works</h3>
-    </div>
-  )
+// export const InputBox = props => {
+//   return (
+//     <div>
+//       <h3>Not yet an input box but I exist to try and test if sharedb works</h3>
+//       <input
+//         type="text"
+//         value="Some value" //{this.state.value}
+//         onChange="" //{this.handleChange}
+//       />
+//     </div>
+//   )
+// }
+
+export class InputBox extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputValue: 'some string'
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      //[event.target.name]: event.target.value
+      inputValue: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  render() {
+    let main = (
+      <div>
+        <h3>
+          Not yet an input box but I exist to try and test if sharedb works
+        </h3>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <p>{this.state.inputValue}</p>
+      </div>
+    )
+
+    return main
+  }
 }
