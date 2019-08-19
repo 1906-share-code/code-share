36b8684c94d78c65be98d5df99af70e30e80728c
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-19 13:57:14 -0500

    minimal working editor in code mirror

--- client/components/Editor.js
+++ client/components/Editor.js
@@ -1,16 +1,29 @@
 import React from 'React'
-import {UnControlled} from 'react-codemirror2'
+import {Controlled} from 'react-codemirror2'
 
 export class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.options = {
+      mode: 'javascript',
+      lineNumbers: true
+    }
+    this.state = {
+      value: 'console.log("Hello World")'
+    }
+  }
+
   render() {
     console.log(this.state)
     let main = (
-      <UnControlled
-        value="console.log(&quot;hello Jon&quot;)"
-        options={{
-          mode: 'javascript',
-          lineNumbers: true
+      <Controlled
+        value={this.state.value}
+        options={this.options}
+        onBeforeChange={(editor, data, value) => {
+          this.setState({value})
         }}
+        onChange={(editor, data, value) => {}}
       />
     )
 
