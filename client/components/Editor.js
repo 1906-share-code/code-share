cbb85127332d77c82725d3c5b950dff99bf4cbba
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-19 14:54:58 -0500

    before any binding

--- client/components/Editor.js
+++ client/components/Editor.js
@@ -2,10 +2,14 @@ import React from 'React'
 import {Controlled} from 'react-codemirror2'
 
 require('codemirror/mode/javascript/javascript')
+var sharedb = require('sharedb/lib/client')
+//import StringBinding from 'sharedb-string-binding'
 
 export class Editor extends React.Component {
   constructor(props) {
     super(props)
+
+    this.codebox = React.createRef()
 
     this.options = {
       mode: 'javascript',
@@ -16,10 +20,33 @@ export class Editor extends React.Component {
     }
   }
 
+  componentDidMount() {
+    let input = this.codebox.current
+    const str = window.location.protocol === 'https:' ? 'wss://' : 'ws://'
+    const socket = new WebSocket(str + window.location.host)
+
+    let connection = new sharedb.Connection(socket)
+    let doc = connection.get('demo', 'inputbox')
+
+    doc.subscribe(function(err) {
+      if (err) throw err
+      console.log('before ', doc.type)
+      if (doc.type === null) {
+        console.log('I am running ', doc.type)
+        doc.create({content: ''})
+      }
+      console.log('after', doc.type)
+      //let binding = new StringBinding(input, doc, ['content'])
+      //binding.setup()
+    })
+  }
+
   render() {
     console.log(this.state)
     let main = (
       <Controlled
+        ref={this.codebox}
+        type="text"
         value={this.state.value}
         options={this.options}
         onBeforeChange={(editor, data, value) => {
