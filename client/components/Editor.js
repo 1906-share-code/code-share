6006b41e9a2a061bc499a57aaccbc19c5d0bc0ae
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-22 16:45:25 -0500

    changed react to lower case

--- client/components/Editor.js
+++ client/components/Editor.js
@@ -1,4 +1,4 @@
-import React from 'React'
+import React from 'react'
 import {UnControlled} from 'react-codemirror2'
 import {transformCodeMirrorChange} from '../javascriptstuff/otoffsetfuncs'
 import connection from '../connect'
