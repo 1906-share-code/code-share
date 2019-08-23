d29c74e45b5b69aeaaa5eb7e72b50a31d9d4f2b8
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-23 14:13:51 -0500

    remove time stamps from dbs

--- server/db/models/ops.js
+++ server/db/models/ops.js
@@ -1,26 +1,30 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Ops = db.define('ops', {
-  collection: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    primaryKey: true
+const Ops = db.define(
+  'ops',
+  {
+    collection: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    doc_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    version: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    operation: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    }
   },
-  doc_id: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    primaryKey: true
-  },
-  version: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    primaryKey: true
-  },
-  operation: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  }
-})
+  {timestamps: false}
+)
 
 module.exports = Ops
--- server/db/models/snapshots.js
+++ server/db/models/snapshots.js
@@ -1,29 +1,33 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Snapshots = db.define('snapshots', {
-  collection: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    primaryKey: true
+const Snapshots = db.define(
+  'snapshots',
+  {
+    collection: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    doc_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
+    doc_type: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    version: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    data: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    }
   },
-  doc_id: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    primaryKey: true
-  },
-  doc_type: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  version: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  data: {
-    type: Sequelize.TEXT,
-    allowNull: false
-  }
-})
+  {timestamps: false}
+)
 
 module.exports = Snapshots
