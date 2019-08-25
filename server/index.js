045cdefccac215a8cd85fbaf3ec90efb8949de50
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-08-24 21:30:59 -0500

    a save mongodb runs without crashing but not connected yet

--- server/index.js
+++ server/index.js
@@ -15,6 +15,21 @@ const {ConnectionStringParser} = require('connection-string-parser')
 const {type} = require('ot-text')
 const ShareDB = require('sharedb')
 ShareDB.types.register(type)
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const url = 'mongodb://localhost:27017'
+const dbName = 'code-share'
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err)
+  console.log('Connected successfully to server in mongodb')
+
+  const db = client.db(dbName)
+
+  client.close()
+})
+
+// Connection URL
 // const connectionStringParser = new ConnectionStringParser({
 //   scheme: 'postgres',
 //   hosts: []
